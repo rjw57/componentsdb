93d426a0ec0d18b84352f77256b241b435e34524
Rich Wareham <rjw57@cam.ac.uk>
2015-07-20 16:27:10 +0100

    use reflection to populate DB model

--- componentsdb/model.py
+++ componentsdb/model.py
@@ -2,16 +2,18 @@
 SQLAlchemy models for the database.
 
 """
+from sqlalchemy.ext.declarative import DeferredReflection
 from flask.ext.sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+from componentsdb.app import app
+
+db = SQLAlchemy(app)
+db.reflect()
 
 class Component(db.Model):
-    __tablename__ = 'components'
-
-    id = db.Column(db.Integer, primary_key=True)
-    code = db.Column(db.Text)
-    description = db.Column(db.Text)
-    datasheet_url = db.Column(db.Text)
-    created_at = db.Column(db.DateTime, server_default=db.FetchedValue())
-    updated_at = db.Column(db.DateTime, server_default=db.FetchedValue())
+    __table__ = db.Table(
+        'components', db.metadata,
+        db.Column('created_at', db.DateTime, server_default=db.FetchedValue()),
+        db.Column('updated_at', db.DateTime, server_default=db.FetchedValue()),
+        extend_existing=True,
+    )
--- tests/conftest.py
+++ tests/conftest.py
@@ -5,23 +5,20 @@ pytest configuration and fixtures
 import pytest
 
 from faker import Faker
-from flask import Flask
 from mixer.backend.flask import Mixer
 
+from componentsdb.app import app as _app
 from componentsdb.model import db as _db, Component
 
 @pytest.fixture(scope='module')
 def app():
-    app = Flask(__name__)
-    app.debug = True
-    app.config['TESTING'] = True
-    return app
+    _app.debug = True
+    _app.config['TESTING'] = True
+    return _app
 
 @pytest.fixture(scope='module')
 def db(app):
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres:///comp_testing'
     app.config['SQLALCHEMY_ECHO'] = True
-    _db.init_app(app)
     return _db
 
 @pytest.fixture(scope='module')
