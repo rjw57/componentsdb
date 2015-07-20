e74e211a1da2d893d0cfcf1c89df4f53ce42f6a9
Rich Wareham <rjw57@cam.ac.uk>
2015-07-20 21:34:46 +0100

    add simple flask-script based management utility

---
+++ manage.py
@@ -0,0 +1,18 @@
+"""
+CLI management script. (Requires flask-script.)
+
+"""
+from flask.ext.script import Manager
+
+from componentsdb.app import create_app
+from componentsdb.model import db
+
+app = create_app()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres:///comp_testing'
+app.config['SQLALCHEMY_ECHO'] = True
+db.init_app(app)
+manager = Manager(app)
+
+if __name__ == "__main__":
+    manager.run()
+
