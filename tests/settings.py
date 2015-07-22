f58ae88ebfc100a60c06708daf9547ba50858a00
Rich Wareham <rjw57@cam.ac.uk>
2015-07-22 16:51:17 +0100

    add magic to generate self-signed certs on each run of the test suite

--- tests/settings.py
+++ tests/settings.py
@@ -3,6 +3,13 @@ Settings for application when being run in the test suite.
 
 """
 import os
+import sys
+
+# Add the directory containing this file to the search path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+# Import function to generate a self-signed cert dynamically
+from x509cert import gen_self_signed_cert
 
 DEBUG = True
 TESTING = True
@@ -14,3 +21,8 @@ SQLALCHEMY_DATABASE_URI = os.environ.get(
     'COMPONENTSDB_DATABASE_URI', 'sqlite://'
 )
 SQLALCHEMY_ECHO = True
+
+_cert, _key = gen_self_signed_cert()
+GOOGLE_OAUTH2_CERTS = {'selfsigned': _cert}
+TESTING_GOOGLE_OAUTH2_CERT_PRIV_KEYS = {'selfsigned', _key}
+
---
+++ tests/x509cert.py
@@ -0,0 +1,47 @@
+import logging
+import os
+import shutil
+import subprocess
+import tempfile
+from contextlib import contextmanager
+
+_LOG = logging.getLogger(__name__)
+
+@contextmanager
+def _temp_dir():
+    """Context manager which creates termporary directory and clears up
+    after."""
+    d = tempfile.mkdtemp(prefix='self-signed-cert-')
+    yield d
+    shutil.rmtree(d)
+
+def gen_self_signed_cert():
+    with _temp_dir() as d:
+        key_fn = os.path.join(d, 'key.pem')
+        crt_fn = os.path.join(d, 'cert.pem')
+
+        _LOG.info('Generating X509 self-signed certificate.')
+
+        output = subprocess.check_output(
+            'openssl req -batch -new -newkey rsa:2048 -days 365 -nodes -x509'.split() +
+            ['-keyout', key_fn, '-out', crt_fn],
+            stderr=subprocess.STDOUT, cwd=d
+        )
+
+        for l in output.splitlines():
+            _LOG.info(l.decode('utf8'))
+
+        with open(key_fn) as k, open(crt_fn) as c:
+            return k.read(), c.read()
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+
+    cert, key = gen_self_signed_cert()
+    print('Certificate:')
+    print(cert)
+    print('Key:')
+    print(key)
+
+if __name__ == '__main__':
+    main()
