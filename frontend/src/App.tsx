aa43d81825b6604897fe92331038f4257c10ef96
Rich Wareham <rich@richwareham.com>
2024-07-26 21:51:41 +0100

    feat: add graphql IDE to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "graphiql/graphiql.css";
+
+import { GraphiQL } from "graphiql";
+import { createGraphiQLFetcher } from "@graphiql/toolkit";
+
+const fetcher = createGraphiQLFetcher({
+  url: "/graphql",
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <GraphiQL fetcher={fetcher} />;
 }
 
 export default App;
