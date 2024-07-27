82e9c0d224e1d5380e8ec8811c56c00d899661b8
Rich Wareham <rich@richwareham.com>
2024-07-27 09:40:04 +0100

    feat: add simple sign in with Google button to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,27 @@ import "graphiql/graphiql.css";
 
 import { GraphiQL } from "graphiql";
 import { createGraphiQLFetcher } from "@graphiql/toolkit";
+import { GoogleLogin } from "@react-oauth/google";
 
 const fetcher = createGraphiQLFetcher({
   url: "/graphql",
 });
 
 function App() {
-  return <GraphiQL fetcher={fetcher} />;
+  return (
+    <>
+      <GoogleLogin
+        onSuccess={(credentialResponse) => {
+          console.log(credentialResponse);
+        }}
+        onError={() => {
+          console.log("Login Failed");
+        }}
+        use_fedcm_for_prompt={true}
+      />
+      <GraphiQL fetcher={fetcher} />
+    </>
+  );
 }
 
 export default App;
