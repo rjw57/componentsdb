9acf4531a1834fd732913be6d208fc0b384db174
Rich Wareham <rich@richwareham.com>
2024-07-29 17:08:00 +0100

    feat: make graphiql page look nicer

--- frontend/src/components/GraphiQLPage.tsx
+++ frontend/src/components/GraphiQLPage.tsx
@@ -1,6 +1,6 @@
 import "graphiql/graphiql.min.css";
 
-import { Layout } from "antd";
+import { Layout, theme } from "antd";
 import { GraphiQL } from "graphiql";
 import { createGraphiQLFetcher } from "@graphiql/toolkit";
 
@@ -13,16 +13,21 @@ const fetcher = createGraphiQLFetcher({
   url: "/graphql",
 });
 
-export const GraphiQLPage = () => {
+export const GraphiQLPage: React.FC = () => {
+  const {
+    token: { borderRadiusLG },
+  } = theme.useToken();
   const { data: providers } = useFederatedIdentitiyProviders({ client: unauthenticatedClient });
+  console.log(providers);
   return (
     <Page>
       <Layout style={{ height: "100vh" }}>
         <Layout.Header>
           <PageHeader />
         </Layout.Header>
-        <Layout.Content>
-          <div>{JSON.stringify(providers)}</div>
+        <Layout.Content
+          style={{ margin: "32px 48px", overflow: "auto", borderRadius: borderRadiusLG }}
+        >
           <GraphiQL fetcher={fetcher} />
         </Layout.Content>
       </Layout>
