9fb92dcf959d9fdf1ca2813e2f92313ac3e176d0
Rich Wareham <rich@richwareham.com>
2024-07-30 09:39:11 +0100

    feat: add some basic query complexity limitation

--- backend/componentsdb/graphql/__init__.py
+++ backend/componentsdb/graphql/__init__.py
@@ -1,8 +1,14 @@
 import strawberry
+from strawberry.extensions import MaxAliasesLimiter, MaxTokensLimiter, QueryDepthLimiter
 
 from .context import make_context
 from .types import Mutation, Query
 
 __all__ = ["schema", "make_context"]
 
-schema = strawberry.Schema(query=Query, mutation=Mutation)
+
+schema = strawberry.Schema(
+    query=Query,
+    mutation=Mutation,
+    extensions=[QueryDepthLimiter(max_depth=15), MaxTokensLimiter(1000), MaxAliasesLimiter(10)],
+)
