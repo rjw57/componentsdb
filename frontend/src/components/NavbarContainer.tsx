0fa69d9aa4762dd26f14b82f4465ca54e7787581
Rich Wareham <rich@richwareham.com>
2024-08-01 19:24:16 +0100

    fix: make sure active path is shown in production

--- frontend/src/components/NavbarContainer.tsx
+++ frontend/src/components/NavbarContainer.tsx
@@ -15,28 +15,35 @@ export const NavbarDynamicContent = () => (
   </>
 );
 
-export const NavbarContainer: React.FC<NavbarContainerProps> = ({ pathname = "", children }) => (
-  <Navbar fluid rounded>
-    <Navbar.Brand className="pr-8">
-      <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-        Components DB
-      </span>
-    </Navbar.Brand>
-    <Navbar.Collapse>
-      <Navbar.Link href="/" active={pathname === "/"}>
-        Home
-      </Navbar.Link>
-      <Navbar.Link href="/api" active={pathname === "/api"}>
-        GraphQL
-      </Navbar.Link>
-    </Navbar.Collapse>
-    <div className="flex-grow" />
-    <div className="flex gap-2 items-center">
-      <DarkThemeToggle />
-      {children}
-      <Navbar.Toggle />
-    </div>
-  </Navbar>
-);
+export const NavbarContainer: React.FC<NavbarContainerProps> = ({ pathname = "", children }) => {
+  // Trim a triling '/' from the pathname unless it is also the leading '/'. We need to do this
+  // because when rendering for static generation, pathnames have trailing '/'s but when generating
+  // in local dev they do not.
+  const trimmedPathname = pathname.replace(/^(.+)\/$/, (_, p1) => p1);
+
+  const NavbarLink: React.FC<Omit<React.ComponentProps<typeof Navbar.Link>, "active">> = (
+    props,
+  ) => <Navbar.Link {...props} active={trimmedPathname === props.href} />;
+
+  return (
+    <Navbar fluid rounded>
+      <Navbar.Brand className="pr-8">
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+          Components DB
+        </span>
+      </Navbar.Brand>
+      <Navbar.Collapse>
+        <NavbarLink href="/">Home</NavbarLink>
+        <NavbarLink href="/api">GraphQL</NavbarLink>
+      </Navbar.Collapse>
+      <div className="flex-grow" />
+      <div className="flex gap-2 items-center">
+        <DarkThemeToggle />
+        {children}
+        <Navbar.Toggle />
+      </div>
+    </Navbar>
+  );
+};
 
 export default NavbarContainer;
