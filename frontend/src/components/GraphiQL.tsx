29cd683310a080f5e3a4f3d1b90e501e2f2946a5
Rich Wareham <rich@richwareham.com>
2024-08-03 22:19:08 +0100

    feat: make graphiql mode match selected theme

--- frontend/src/components/GraphiQL.tsx
+++ frontend/src/components/GraphiQL.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "graphiql/graphiql.min.css";
 import { GraphiQL as WrappedGraphiQL } from "graphiql";
 import { createGraphiQLFetcher } from "@graphiql/toolkit";
+import { useThemeMode } from "flowbite-react";
 
 import { graphQLUrl } from "../config";
 import useSignIn from "../hooks/useSignIn";
@@ -10,6 +11,10 @@ export const GraphiQL: React.FC<Omit<React.ComponentProps<typeof WrappedGraphiQL
   props,
 ) => {
   const { authenticatedFetch } = useSignIn();
+
+  const { mode: themeMode } = useThemeMode();
+  const forcedMode = themeMode == "auto" ? "system" : themeMode;
+
   const fetcher = React.useMemo(() => {
     return createGraphiQLFetcher({
       url: graphQLUrl,
@@ -17,7 +22,7 @@ export const GraphiQL: React.FC<Omit<React.ComponentProps<typeof WrappedGraphiQL
     });
   }, [authenticatedFetch]);
 
-  return <WrappedGraphiQL fetcher={fetcher} {...props} />;
+  return <WrappedGraphiQL forcedTheme={forcedMode} fetcher={fetcher} {...props} />;
 };
 
 export default GraphiQL;
